68fa59c04b9db5ff0f9a97c589caee37db6c8a59
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-26 20:15:46 -0300

    added trim so it dont matter the OS

--- test/LibraryTests/SingletonTests.cs
+++ test/LibraryTests/SingletonTests.cs
@@ -11,15 +11,14 @@ namespace LibraryTests
         /// en la consola, para "ver" lo que se escribe en la consola es necesario "redirigir" la salida en la consola
         ///  utilizando <see cref="System.Console.SetOut(TextWriter)"/> a un <see cref="StringWriter"/>.
         /// </summary>
-        [TestCase("Hello World!\r\n")]
-        [TestCase("Hello World!\n")]
+        [TestCase("Hello World!")]
         public void TestSayHiToTheWorld(string expected)
         {
             using (StringWriter sw = new StringWriter())
             {
                 System.Console.SetOut(sw);
                 Singleton<Greeter>.GetInstance().SayHiToTheWorld();
-                Assert.That(sw.ToString(), Is.EqualTo(expected));
+                Assert.That(sw.ToString().TrimEnd(), Is.EqualTo(expected));
             }
         }
     }
